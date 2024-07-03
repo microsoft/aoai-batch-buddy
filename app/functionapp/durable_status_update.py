22024ba954caf272d0be2948e0b3183e9662f33c
Phil Jirsa <phil.jirsa@microsoft.com>
2024-07-03 20:07:20 +0000

    upload output file to outputfiles container

--- app/functionapp/durable_status_update.py
+++ app/functionapp/durable_status_update.py
@@ -2,6 +2,9 @@ import logging
 import azure.functions as func
 import azure.durable_functions as df
 import openai
+import json
+import os
+from azure.storage.blob import BlobServiceClient
 
 status_update = df.Blueprint()
 
@@ -16,16 +19,48 @@ async def start_orchestrator(req: func.TimerRequest, client):
     )
     
     results = batch_client.batches.list(limit=10)
-    for job in results:
-        logging.info(f"Job {i}: {job}")
-        print(job)
-        #await client.start_new("my_orchestrator", client_input=job)
+    payload = json.dumps(results.to_dict()["value"])
     
-    #instance_id = await client.start_new("my_orchestrator", client_input=payload)
+    instance_id = await client.start_new("my_orchestrator", client_input=payload)
     
     #logging.info(f"Started orchestration with ID = '{instance_id}'.")
         
 
 @status_update.orchestration_trigger(context_name="context")
 def my_orchestrator(context: df.DurableOrchestrationContext):
-    return context.get_input()
+    input = context.get_input()
+    jobs = json.loads(input)
+    
+    # fan out - start a new activity function for each job
+    tasks = []
+    for j in jobs:
+        tasks.append(context.call_activity(name="update_status", input_=json.dumps(j)))
+    
+    status_results = yield context.task_all(tasks)
+    
+    return status_results
+
+@status_update.activity_trigger("jobstr", "update_status")
+def update_status(jobstr: str):
+    # Update the status of the job
+    job = json.loads(jobstr)
+    logging.info(f"Updating status for job {job['id']}")
+    print(job['status'])
+    
+    # if job is complete, store the output file in blob storage
+    if job["status"] == "Completed":
+        # get the file from openai
+        file_client = openai.AzureOpenAI(
+        api_version="2024-04-15-preview"
+    )
+        file = file_client.files.content(job["output_file_id"])
+        conn_str = os.environ["BlobStorageConnectionString"]
+        # store the output file in blob storage
+        blob_client = BlobServiceClient.from_connection_string(conn_str)
+        container_client = blob_client.get_container_client("outputfiles")        
+            
+        container_client.upload_blob(name=f"{job['output_file_id']}.jsonl", data=file.content, overwrite=True)
+    
+    # give cosmos the 411
+    
+    return job
